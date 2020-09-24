f4558bfc00c73822673a8da2b5d0fd2110258b3c
Findu <arttu.hiekkanen@hotmail.com>
2020-09-25 00:39:43 +0300

    Remade the entire command system, more scalable now

---
+++ commands/holo.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hololive (ホロライブ), is an agency of Virtual YouTubers created by Cover Corp").catch(console.error);
+}
--- main.js
+++ main.js
@@ -1,22 +1,48 @@
+const fs = require('fs');
 const Discord = require("discord.js");
+const Enmap = require("enmap");
+
 const config = require("./config.json");
-const tokenfile = require("./token.json");
-const prefix = "!";
+const tokenFile = require("./token.json");
 
 const client = new Discord.Client();
 
-client.on("message", function(message) {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
+client.config = config;
 
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
 
-    if (command === "hello"){
-        const ping = Date.now() - message.createdTimestamp;
-        message.reply(`AAaaAAaaaaaaAAA, ${ping}`);
-    }
+        client.on(eventName, event.bind(null, client));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
 });
 
-client.login(tokenfile.TOKEN);
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        console.log(`Loading command ${commandName}`);
+        // Here we simply store the whole thing in the command Enmap. We're not running it right now.
+        client.commands.set(commandName, props);
+    });
+});
+
+client.once('ready', () => {
+    console.log('Ready')
+    client.user.setActivity(`草`)
+})
+
+// console logging
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+
+client.login(tokenFile.TOKEN);
